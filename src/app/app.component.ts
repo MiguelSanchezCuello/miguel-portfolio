98b1fd3bbcd350714dd398a4a4f7eca184eab447
msanchez <miguelsc10@hotmail.com>
2026-03-18 21:34:34 -0400

    Configurar Tailwind v3 con PrimeUI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ButtonModule, MenubarModule,
+    BadgeModule, AvatarModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'miguel-portfolio';
+  items: MenuItem[] | undefined;
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Home',
+                icon: 'pi pi-home'
+            },
+            {
+                label: 'Features',
+                icon: 'pi pi-star'
+            },
+            {
+                label: 'Projects',
+                icon: 'pi pi-search',
+                items: [
+                    {
+                        label: 'Core',
+                        icon: 'pi pi-bolt',
+                        shortcut: '⌘+S'
+                    },
+                    {
+                        label: 'Blocks',
+                        icon: 'pi pi-server',
+                        shortcut: '⌘+B'
+                    },
+                    {
+                        label: 'UI Kit',
+                        icon: 'pi pi-pencil',
+                        shortcut: '⌘+U'
+                    },
+                    {
+                        separator: true
+                    },
+                    {
+                        label: 'Templates',
+                        icon: 'pi pi-palette',
+                        items: [
+                            {
+                                label: 'Apollo',
+                                icon: 'pi pi-palette',
+                                badge: '2'
+                            },
+                            {
+                                label: 'Ultima',
+                                icon: 'pi pi-palette',
+                                badge: '3'
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                label: 'Contact',
+                icon: 'pi pi-envelope',
+                badge: '3'
+            }
+        ];
+    }
 }
